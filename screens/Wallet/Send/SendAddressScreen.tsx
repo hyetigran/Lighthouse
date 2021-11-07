803460a3d477756f1f9a73a9ae231e09c56e92df
Tigran Asriyan <tigran.asriyan@gmail.com>
2021-11-07 15:57:20 +0400

    styled send screen

--- screens/Wallet/Send/SendAddressScreen.tsx
+++ screens/Wallet/Send/SendAddressScreen.tsx
@@ -1,10 +1,52 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Input } from "react-native-elements";
+import { Ionicons } from "@expo/vector-icons";
+import Colors from "../../../constants/Colors";
+
+const ADDRESS_INPUT_LABEL = "Enter bitcoin address";
+
+const { background, darkGrey, gainGreenLite, text } = Colors.light;
 
 const SendAddressScreen = () => {
+  const [sendAddress, setSendAddress] = useState("");
+  // FROM WALLET OPTION
+
+  const changeAddressHandler = (value: string) => {};
+
+  const pasteClipboardHandler = () => {};
+
+  const qrScannerHandler = () => {};
+
   return (
     <View style={styles.container}>
-      <Text>SendAddressScreen</Text>
+      {/* <View>
+        <Text>Navigated FROM WALLET</Text>
+      </View> */}
+      <View>
+        <Input
+          containerStyle={styles.addressInputContainer}
+          inputContainerStyle={styles.addressInput}
+          placeholder={ADDRESS_INPUT_LABEL}
+          value={sendAddress}
+          onChangeText={changeAddressHandler}
+        />
+      </View>
+      <View style={styles.optionSelectContainer}>
+        <TouchableOpacity
+          style={styles.copyClipboardContainer}
+          onPress={pasteClipboardHandler}
+        >
+          <Ionicons size={18} color={text} name={"clipboard-outline"} />
+          <Text>Paste Clipboard</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TouchableOpacity style={styles.scanButton} onPress={qrScannerHandler}>
+          <Ionicons size={20} color={background} name={"qr-code-outline"} />
+          <Text style={styles.scanButtonText}>Scan QR Code</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -12,6 +54,45 @@ const SendAddressScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 50,
+    marginBottom: 350,
+    marginHorizontal: 30,
+    justifyContent: "space-around",
+    minHeight: 350,
+  },
+  addressInputContainer: {
+    paddingHorizontal: 0,
+  },
+  addressInput: {
+    borderWidth: 1,
+    borderColor: darkGrey,
+    borderRadius: 4,
+    paddingHorizontal: 10,
+  },
+  scanButton: {
+    backgroundColor: gainGreenLite,
+    flexDirection: "row",
+    borderRadius: 4,
+    padding: 20,
+    justifyContent: "center",
+  },
+  scanButtonText: {
+    color: background,
+    fontWeight: "bold",
+    fontSize: 18,
+    paddingLeft: 6,
+  },
+  optionSelectContainer: {
+    marginBottom: 20,
+    alignItems: "center",
+  },
+  copyClipboardContainer: {
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: darkGrey,
+    padding: 10,
+    alignItems: "center",
+    width: 130,
   },
 });
 
